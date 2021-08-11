7cf3f9c46cba6f564b30fe30b9f5fff66daabcfe
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-08-11 18:32:32 -0400

    First React w/JSX

---
+++ 2_react/1_intro_to_react/FR with JSX/first_react_with_jsx/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+        <ul>
+          <li>&#9672; Read/listen to Scripture</li>
+          <li>&#9672; Feed Smokie</li>
+          <li>&#9672; Take son 1 to work</li>
+          <li>&#9672; Take love to work</li>
+          <li>&#9672; Submit Discussion</li>
+          <li>&#9672; Review MERN Lecture</li>
+          <li>&#9672; Take son 2 to work</li>
+          <li>&#9672; Run household errands</li>
+          <li>&#9672; Pick up son 1 from work</li>
+          <li>&#9672; Pick up love from work</li>
+          <li>&#9672; Complete & Submit Assignment</li>
+          <li>&#9672; Feed Smokie</li>
+          <li>&#9672; Cook Dinner</li>
+          <li>&#9672; Attend online Class</li>
+          <li>&#9672; Pick up son 2 from work</li>
+          <li>&#9672; Watch YouTube - Craft Marathon</li>
+          <li>&#9672; Read/listen to Scripture</li>
+          <li>&#9672; Try to fall asleep without dreaming of coding!</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
+
+
+// Resource:
+// https://reactjs.org/docs/dom-elements.html#style
